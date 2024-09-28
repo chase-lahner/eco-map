c08c7f0e9aaff36871e2e04a0e0acf84605ade26
chase-lahner <cal256@pitt.edu>
2024-09-28 17:29:19 -0400

    update

--- demos/eco-map/src/main_page.tsx
+++ demos/eco-map/src/main_page.tsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import { Map, AdvancedMarker } from "@vis.gl/react-google-maps";
 import { renderIntoDocument } from "react-dom/test-utils";
 
+interface checkProp {
+    
+}
 
-function MainPage() {
+const  MainPage: React.FC<checkProp> = () => {
 
     const [RouteCount, setRouteCount] = useState(5);
     const position = {lat: 53.54992, lng: 10.00678};
+    const sourceDest = useState([]);
 
     return (
         <div className="flex flex-row">
@@ -14,8 +18,7 @@ function MainPage() {
             <div className="flex-initial basis-2/6">
                 <h1 className="text-center">Enter Trip Routes</h1>
                 <ul className="menu bg-base-200 rounded-box w-full">
-                    <li><input type="text" placeholder={"Enter Destination " } className="input w-full" /></li>
-                    <li><input type="text" placeholder="Enter Destination " className="input w-full" /></li>
+                    <li><input type="text" placeholder="Enter Source " className="input w-full" /></li>
                     <li><input type="text" placeholder="Enter Destination " className="input w-full" /></li>
 
                 </ul>
